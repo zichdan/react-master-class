eda7ed747928195d903e6f0513df978a9484db7a
zichdan <zichdan1999@gmail.com>
2023-12-28 16:08:33 +0100

    First commit from 27th / 12 / 2023

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Product from './Products'
+
+function App() {
+
+  return (
+   <div>
+    <h1>Hello world</h1>
+   
+   <Product/>
+   </div>
+  )
+
+}
+
+export default App
---
+++ src/Products.jsx
@@ -0,0 +1,11 @@
+
+
+function Product() {
+
+    <P>This is a new Product Page!!!!!</P>
+    
+}
+
+
+
+export default Product;
